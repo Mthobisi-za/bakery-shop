7dda88bff5035729ecd8fa3c4b5c98b73003391c
Mthobisi-za <mthobisimtho333@gmail.com>
2022-02-04 19:58:24 +0200

    added products

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,16 +1,28 @@
 import './Shop.css'
+import Data from './data'
 
 
  function Shop(){
-    let data = ()=>{
-        fetch('./data.json').then(data=>{console.log(data)}).catch(err =>{console.log(err)})
-    } 
-    {data()}
+    
     return(
         <>
         <h1 className="subtitle">Bakery Catalogue <i class="far fa-laugh-wink"></i> </h1>
         <p className="p-cata">Welcome to our fresh products *</p>
             <div className="shop" id='shop'>shop</div>
+            <div className="carryCon">
+                <div className="empty"></div>
+                <div className="products">
+                {Data.map((element, index)=>(
+                   <div className="product">
+                       <h2>Category: {element.category}</h2> <br />
+                       <img src={element.url} alt={element.category} />
+                       <h2>Price: <span className='price'>{element.price}</span></h2>
+                   </div> 
+                ))}
+            </div>
+            <div className="empty"></div>
+            </div>
+           
         </>
     )
 
